ec23911b7cea0ed26675140fe2c9719c07a3b6b6
turboSlowPoke <mega_pokemon@mail.ru>
2018-08-08 17:08:40 +0300

    l;kjlkj

--- js/change_bg.js
+++ js/change_bg.js
@@ -1,12 +1,17 @@
 $(window).scroll(function() {
     var windowY = $(window).height();
-    console.log(windowY);
     var scrolledY = $(window).scrollTop();
-    var image_url = 'images/page03/banner-10.jpg';
 
-    if (scrolledY > windowY+50 && scrolledY  < windowY*3+150) {
+    var image_url = 'images/page03/banner-10.jpg';
+    $('.block01').height(windowY);
+    $('.block02').height(windowY);
+    if ($('.block03').height()<windowY){
+        console.log("$('.block03').height()<windowY===true");
+        $('.block03').height(windowY);
+    }
+    if (scrolledY > windowY*1.1 && scrolledY  < windowY*3.25) {
         image_url = 'images/page03/bg03.jpg';
-    }else if (scrolledY > windowY*3+100){
+    }else if (scrolledY > windowY*3){
         image_url = 'images/page03/willa.jpg';
     }
     $("body").css({'background':"url(" + image_url + ")","background-size": "cover","background-repeat": "no-repeat","background-attachment": "fixed"});
